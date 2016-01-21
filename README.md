be1d431dea5ee2a365795341447ca9d7755966d6
Kevin Thompson <kevin.david.thompson@gmail.com>
2016-01-21 09:35:20 -0800

    adds support for multi-page responses

--- README.md
+++ README.md
@@ -26,9 +26,5 @@ places. Such as
 * Do I really need to enrich the users in a separate process? It was a fun learning exercise but I'm not sure that was actually necessary to make this code perform well.
 * This code has zero protection for rate limiting. In fact, since I enrich the user data in parallel there's a good chance that you'll get rate limited if you have a lot of users that don't have 2FA turned on.
 
-## Shit I need to fix
-
-* I need to figure out how to read the headers that come back from GitHub and get the url for the next page of results. GitHub paginates the answer so right now this is only reading the first page of results. However, parsing the headers was not trivial enough to make it into my proof of concept code.
-
 ## Didn't you write something like this in Golang?
 I sure did. You can find it [here](https://github.com/blackfist/deez_factors). It probably works better too because I was able to lean on code that other people wrote.
--- lib/factor_audit.ex
+++ lib/factor_audit.ex
@@ -4,31 +4,77 @@ defmodule FactorAudit do
     # pass that to a function that pulls information
     # for each user.
     {:ok, users} = UserList.new
-    headers = ["User-Agent": "Elixir",
-      "Content-Type": "application/json",
-      "Authorization": "token #{System.get_env("GITHUB_API_KEY")}"]
+
 
     {_, [org_name|_], _} = OptionParser.parse(args)
     IO.puts "Looking for users in #{org_name} org that do not have two factor authentication enabled"
 
     whitelist = WhiteList.read("whitelist.txt")
 
-    response = HTTPotion.get make_url(org_name), [headers: headers]
-
-    case Poison.Parser.parse(response.body) do
-      {:ok, json} ->
-        json |>
-        Enum.filter(fn(x) -> !Enum.member?(whitelist, x["login"]) end) |>
-        Enum.each(fn(x) -> UserList.add(users, x["login"]) end)
-      _ ->
-        IO.puts "Something wrong with the response"
-    end
+    go_get_users(make_url(org_name), users, whitelist)
 
     :timer.sleep(1000)
     UserList.get(users) |> Enum.each(fn(x) -> IO.puts("#{elem(x, 0)}, #{elem(x, 1)}, #{elem(x, 2)}") end)
   end
 
-  def make_url(org_name) do
-    "https://api.github.com/orgs/" <> org_name <> "/members?filter=2fa_disabled"
+  defp go_get_users(url, user_list, whitelist) do
+    headers = ["User-Agent": "Elixir",
+      "Content-Type": "application/json",
+      "Authorization": "token #{System.get_env("GITHUB_API_KEY")}"]
+
+    response = HTTPotion.get url, [headers: headers]
+
+    case Poison.Parser.parse(response.body) do
+      {:ok, json} ->
+        json |>
+        Enum.filter(fn(x) -> !Enum.member?(whitelist, x["login"]) end) |>
+        Enum.each(fn(x) -> UserList.add(user_list, x["login"]) end)
+      _ ->
+        IO.puts "Something wrong with the response"
+    end
+
+    case get_next_link(response.headers) do
+      {:ok, next_link} ->
+        IO.puts "got next page #{next_link}"
+        go_get_users(next_link, user_list, whitelist)
+      _ ->
+        {:done}
+    end
+  end
+
+  defp make_url(org_name) do
+    "https://api.github.com/orgs/" <> org_name <> "/members"
+  end
+
+  def get_next_link(headers) do
+    list_of_links = headers
+      |> Enum.filter(fn(x) -> elem(x, 0) == :"Link" end)
+
+    if length(list_of_links) > 0 do
+      {:"Link",string_of_links} = list_of_links
+      |> hd
+
+      new_list_of_links = string_of_links
+      |> String.split(",")
+      |> Enum.map(fn(x) -> String.split(x) |> List.to_tuple end)
+      |> Enum.filter(fn(x) -> elem(x,1) == "rel=\"next\"" end)
+
+      if length(new_list_of_links) > 0 do
+        {final_link_string, _} = new_list_of_links |> hd
+
+        clean_link = final_link_string
+        |> String.split(["<",">"])
+        |> Enum.filter(fn(x) -> String.starts_with?(x, "https") end)
+        |> hd
+
+        {:ok, clean_link}
+      else
+        IO.puts "No more pages"
+        {:error, :nolink}
+      end
+    else
+      IO.puts "No more pages"
+      {:error, :nolink}
+    end
   end
 end
